3dd355b7a5a146ed07f1d4da23ba20b685470f86
cc1005 <conorkampbell@hotmail.com>
2021-02-19 18:26:45 +0000

    Added a level loader script to move the splash screen onto the start screen.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadStartScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadStartScene()
+    {
+        StartCoroutine(loadStartSceneWithDelay());
+    }
+
+    private IEnumerator loadStartSceneWithDelay()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+}
